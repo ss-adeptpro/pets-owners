53b93d2c5847100a3ab0056206a9b46c7be44a89
sandeep <sk.ajaxian@gmail.com>
2023-12-06 01:04:18 +1100

    AZURE proxy for 3rd party API

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     //setup proxy to handle CORS issue
+    //https://pets-owners.vercel.app/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json
+    //http://127.0.0.1:5173/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json
     proxy: {
-      "/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2": "https://gist.githubusercontent.com/",
+      "/api": "https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/",
     }
   },
   plugins: [react()],
